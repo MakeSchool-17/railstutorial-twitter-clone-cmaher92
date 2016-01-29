0e820d05cb4534b6a2c41ea26018174fc5c04c6e
Connor Maher <connormmaher@gmail.com>
2016-01-28 21:33:31 -0800

    Added some stuff to readme.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,20 +7,20 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :home
     # ensure we receive a success response from the server
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
 end
