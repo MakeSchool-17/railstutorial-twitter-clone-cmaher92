5da0a3e82a8e035352d123bdf4c193907f455c2f
Connor Maher <connormmaher@gmail.com>
2016-02-11 12:11:46 -0800

    Finished email password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   root             'static_pages#home'
@@ -11,6 +15,7 @@ Rails.application.routes.draw do
   delete 'logout'=> 'sessions#destroy'
   resources :account_activations, only: [:edit]
   resources :users # create destroy update new routes for user
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
 #  These are the available pages
 
